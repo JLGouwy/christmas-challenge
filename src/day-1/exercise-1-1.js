eb2b40c2e37abd70b908043c3387413f25c7a659
Jean-louis Gouwy <jean-louisgouwy@MacBook-Pro-de-Jean-louis.local>
2018-12-06 16:49:41 +0100

    first day

---
+++ src/day-1/exercise-1-1.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+fs.readFile('./puzzle.txt', 'utf-8', (err, file) => {
+  const lines = file.split('\n');
+
+  const sumResult = lines.reduce(
+    (previousValue, currentValue) => currentValue + parseInt(previousValue, 10),
+    0,
+  );
+  console.log(`resultat: ${sumResult}`);
+});
