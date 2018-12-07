beed10318c41fee07d926b6e32cba65d0cff9eb6
Jean-louis Gouwy <jean-louisgouwy@MacBook-Pro-de-Jean-louis.local>
2018-12-07 10:45:47 +0100

    day1-part2: replace array by Set Object

--- src/day-1/part-2.js
+++ src/day-1/part-2.js
@@ -2,7 +2,13 @@ const fs = require('fs');
 
 const contentFile = fs.readFileSync('puzzle.txt');
 const lines = contentFile.toString().split('\n');
-const uniqueFrequencies = [];
+/**
+ * At first, I used a simple Array with push and includes.
+ * The execution of the script : node part-2.js  14,81s user 0,04s system 99% cpu 14,884 total
+ * Then, I used the Set object.
+ * The execution of the script : node part-2.js  0,09s user 0,02s system 99% cpu 0,108 total
+ */
+const uniqueFrequencies = new Set();
 let duplicateFound = false;
 let i = 0;
 let currentValue = 0;
@@ -14,9 +20,9 @@ while (!duplicateFound) {
 
   currentValue += parseInt(lines[i], 10);
 
-  duplicateFound = uniqueFrequencies.includes(currentValue);
+  duplicateFound = uniqueFrequencies.has(currentValue); // instead of includes().
 
-  uniqueFrequencies.push(currentValue);
+  uniqueFrequencies.add(currentValue); // instead of push().
 
   i += 1;
 }
